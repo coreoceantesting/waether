b8ce88ebcda5d9968069b9b7006c7d480e5b0eea
coreoceantesting <coreoceantesting@gmail.com>
2024-07-15 15:40:41 +0530

    fix api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,7 +11,7 @@ class ApiController extends Controller
 {
     public function store(Request $request)
     {
-        // Log::info($request);
+        Log::info($request);
         DB::beginTransaction();
         try {
             $datas = $request->all();
@@ -23,7 +23,40 @@ class ApiController extends Controller
                     ->first();
 
                 if ($check) {
-                    Weather::create($data);
+                    Weather::create([
+                        'location_id' => $data['location_id'],
+                        'date' => $data['date'],
+                        'time' => $data['time'],
+                        'temp_out' => $data['temp_out'],
+                        'hi_temp' => $data['hi_temp'],
+                        'low_temp' => $data['low_temp'],
+                        'out_hum' => $data['out_hum'],
+                        'dew_pt' => $data['dew_pt'],
+                        'wind_speed' => $data['wind_speed'],
+                        'wind_dir' => $data['wind_dir'],
+                        'wind_run' => $data['wind_run'],
+                        'hi_speed' => $data['hi_speed'],
+                        'hi_dir' => $data['hi_dir'],
+                        'wind_chill' => $data['wind_chill'],
+                        'heat_index' => $data['heat_index'],
+                        'thw_index' => $data['thw_index'],
+                        'bar' => $data['bar'],
+                        'rain' => $data['rain'],
+                        'rain_rate' => $data['rain_rate'],
+                        'heat_dd' => $data['heat_dd'],
+                        'cool_dd' => $data['cool_dd'],
+                        'in_temp' => $data['in_temp'],
+                        'in_hum' => $data['in_hum'],
+                        'in_dew' => $data['in_dew'],
+                        'in_heat' => $data['in_heat'],
+                        'in_emc' => $data['in_emc'],
+                        'in_air_destiny' => $data['in_air_destiny'],
+                        'wind_samp' => $data['wind_samp'],
+                        'wind_txt' => $data['wind_txt'],
+                        'iss_recept' => $data['iss_recept'],
+                        'arc_int' => $data['arc_int'],
+                        'datetime' => $data['datetime'],
+                    ]);
                 } else {
                     Weather::where('id', $check->id)->where('location_id', $data['location_id'])
                         ->update([
